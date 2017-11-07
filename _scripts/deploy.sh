af1ae0ac7a0046f5099a9f6b174d0fe7be94402e
KIMB0 <Kimsen1992@gmail.com>
2017-11-07 20:59:26 +0100

    Last test today.

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -2,11 +2,10 @@
 set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
     # Initialize a new git repo in _site, and push it to our server.
-    cd _site
     git init
         
     git remote add deploy "root@165.227.162.254:/var/www/travis-deploy/.git"
-    git config user.name "KIMB0"
+    git config user.name "root"
     git config user.email "kimsen1992@gmail.com"
     
     git add .
