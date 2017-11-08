8f0f450562ac8e5953255bc85c429dc034fdaa2f
KIMB0 <Kimsen1992@gmail.com>
2017-11-08 17:51:33 +0100

    New Travis settings part 1.

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -4,8 +4,8 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
     # Initialize a new git repo in _site, and push it to our server.
     git init
         
-    git remote add deploy "root@165.227.162.254:/var/www/travis-deploy/.git"
-    git config user.name "root"
+    git remote add deploy "root@165.227.162.254:/var/repo/travis.git"
+    git config user.name "KIMB0"
     git config user.email "kimsen1992@gmail.com"
     
     git add .
---
+++ _scripts/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -x # Show the output of the following commands (useful for debugging)
+    
+# Import the SSH deployment key
+openssl aes-256-cbc -K $encrypted_394c2ab74843_key -iv $encrypted_394c2ab74843_iv -in deploy_rsa.enc -out deploy_rsa -d
+eval "$(ssh-agent -s)"
+chmod 600 deploy_rsa
+ssh-add deploy_rsa
