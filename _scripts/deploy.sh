c9f4433df5b02995bc901761fe500003bfc432f9
KIMB0 <Kimsen1992@gmail.com>
2017-11-08 18:30:35 +0100

    Part 5 Travis.

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -2,6 +2,7 @@
 set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
     # Initialize a new git repo in _site, and push it to our server.
+    cd resources
     git init
         
     git remote add deploy "root@165.227.162.254:/var/repo/travis.git"
